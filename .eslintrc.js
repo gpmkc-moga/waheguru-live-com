30e62839fde025134cf235fa3eba0d5ed88827a9
Jasdeep Singh <cingh.jasdeep@gmail.com>
2022-05-02 12:46:01 +0530

    update prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,8 +4,10 @@ module.exports = {
     browser: true,
     node: true,
   },
-  extends: ["@nuxtjs/eslint-config-typescript", "plugin:nuxt/recommended"],
-  plugins: [],
+  extends: [
+    "@nuxtjs/eslint-config-typescript",
+    "plugin:nuxt/recommended",
+    "plugin:prettier/recommended",
+  ],
   // add your custom rules here
-  rules: {},
 };
