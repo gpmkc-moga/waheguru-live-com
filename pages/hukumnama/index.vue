b5d7247e2204c06db881a9b1355fe1c10f524c30
Jasdeep Singh <cingh.jasdeep@gmail.com>
2022-05-11 18:26:56 +0530

    home: add hukumnama button Waheguru Ji!

---
+++ pages/hukumnama/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  asyncData({ redirect }) {
+    redirect(302, "/hukumnama/today");
+  },
+});
+</script>
+
+<style scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,21 @@
 <template>
   <div>
     <home-slider class="w-full" />
+
+    <div class="p-2.5 w-full">
+      <!-- hukumnama button -->
+      <nuxt-link to="/hukumnama/today">
+        <nuxt-picture
+          format="webp"
+          class="flex justify-center"
+          src="daily_hukumnama.jpg"
+          :img-attrs="{
+            class:
+              'w-1/2 md:w-1/3 hover:scale-110 transition-transform duration-1000',
+          }"
+        ></nuxt-picture>
+      </nuxt-link>
+    </div>
     <!-- page keeps refreshing in dev mode! -->
     <div v-if="!isDev" class="onesignal-customlink-container"></div>
   </div>
