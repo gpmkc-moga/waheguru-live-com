ff88ade0ef307a28bf437481cb20a47dbe1956b3
Jasdeep Singh <cingh.jasdeep@gmail.com>
2022-05-27 16:51:20 +0530

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,7 +33,7 @@ export default {
       },
       { name: "format-detection", content: "telephone=no" },
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    link: [{ rel: "icon", type: "image/png", href: "favicon-32x32.png", sizes: "32x32" }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
